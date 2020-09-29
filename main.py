c21347c57dca61d6f01a71dfe2698ef371f1a308
Joshua_Littlefield <csm012018@coderacademy.edu.au>
2020-09-29 02:19:54 +0000

    get methods and news

--- main.py
+++ main.py
@@ -4,26 +4,75 @@
 import json
 
 
-def js_r(filename):
-   with open(filename) as f_in:
-       return(json.load(f_in))
+def read_json(filename):
+   with open(filename) as file:
+       return(json.load(file))
 
-if __name__ == "__main__":
-    my_data = js_r('kanji_data.json')
-    #print(my_data)
-example_dict = {}
+def import_kanji():
     
-for idx,val in enumerate(my_data):
-    del(my_data[idx]['kanji']['video'])
-    del(my_data[idx]['kanji']['strokes'])
-    del(my_data[idx]['radical'])
-    example_dict[idx]= my_data[idx]['examples']
-    del(my_data[idx]['examples'])
-    del(my_data[idx]['references'])
+    kanji_list=[]
+
+    if __name__ == "__main__":
+        kanji_data = read_json('kanji_data.json')
+       
     
-#print(my_data)
+    example_dict = {}
+    
+    for idx,val in enumerate(kanji_data):
+        del(kanji_data[idx]['kanji']['video'])
+        del(kanji_data[idx]['kanji']['strokes'])
+        del(kanji_data[idx]['radical'])
+        example_dict[idx]= kanji_data[idx]['examples']
+        del(kanji_data[idx]['examples'])
+        del(kanji_data[idx]['references'])
+    
+    for idx, val in enumerate(kanji_data):
+        character = kanji_data[idx]['kanji']['character']
+        kunyomi = kanji_data[idx]['kanji']['kunyomi']
+        onyomi = kanji_data[idx]['kanji']['onyomi']
+        meaning = kanji_data[idx]['kanji']['meaning']
+        kanji_list.append(Kanji(character,kunyomi,onyomi,meaning))
+    
+        
+    for k in kanji_list:
+        c= k.get_character()
+        k= k.get_kunyomi()
+        o= k.get_onyomi()
+        m= k.get_meaning()
+        
+        print(c,k,o,m)    
 
 
+def import_news():
+    
+    news_list=[]
+    
+    if __name__ == "__main__":
+        news_data = read_json('news_data.json')
+    
+    #print(news_data)
+    
+    #print(news_data["articles"][1]["description"])
+    
+    for idx, val in enumerate(news_data):
+        author = news_data['articles'][idx]['author']
+        title = news_data['articles'][idx]['title']
+        publish_date = news_data['articles'][idx]['publishedAt']
+        content = news_data['articles'][idx]['description']
+        
+        
+        news_list.append(News(author,title,publish_date,content))
+        
+    for n in news_list:
+        a=n.get_author()
+        t=n.get_title()
+        pd=n.get_publish_date()
+        c=n.get_content()
+        
+        print(a,t,pd,c)
+       
+        
+    
 
 class Lesson:
     def __init__(self,kanji,max_kanji,sentence):
@@ -39,7 +88,7 @@ class Lesson:
     def add_sentence (self,sentence):
         pass
     
-kanji_list=[]            
+            
 
 class Kanji:
     def __init__(self,character,kunyomi,onyomi,meaning):
@@ -66,24 +115,31 @@ class Sentence:
         self.reading = reading
         self.meaning = meaning
 
+class News:
+    def __init__(self,author,title,publish_date,content):
+        self.author = author
+        self.title = title
+        self.publish_date = publish_date
+        self.content = content
+        
+    def get_author (self):
+        return self.author
+        
+    def get_title (self):
+        return self.title
+    
+    def get_publish_date (self):
+        return self.publish_date
+        
+    def get_content (self):
+        return self.content
+        
 
 
-for idx, val in enumerate(my_data):
-    character = my_data[idx]['kanji']['character']
-    kunyomi = my_data[idx]['kanji']['kunyomi']
-    onyomi = my_data[idx]['kanji']['onyomi']
-    meaning = my_data[idx]['kanji']['meaning']
-    kanji_list.append(Kanji(character,kunyomi,onyomi,meaning))
-    
-    
-for k in kanji_list:
-    a= k.get_character()
-    b= k.get_kunyomi()
-    c= k.get_onyomi()
-    d= k.get_meaning()
-    
-    print(a,b,c,d)
-    
+
+
+#import_kanji()
+import_news()
     
 
 
--- news.py
+++ news.py
@@ -1,15 +1,45 @@
 import requests
+import json
 
-url = "https://newsapi.org/v2/top-headlines"
+import io
 
-querystring = {"country":"jp"}
+def write_news():
+    try:
+        to_unicode = unicode
+    except NameError:
+        to_unicode = str
 
-headers = {
-    'x-api-key': "2d2aae6c9cc64cd0a10af6ffb26b8927"
-    }
+    url = "https://newsapi.org/v2/top-headlines"
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+    querystring = {"country":"jp","category":"technology"}
 
-print(response.text)
+    headers = {
+        'x-api-key': "2d2aae6c9cc64cd0a10af6ffb26b8927"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
 
 
+    news_list = json.loads(response.text)
+    
+    with io.open("news_data.json","w", encoding='utf8') as data_file:
+        str_ = json.dumps(news_list,
+                          indent=4, sort_keys=True,
+                          separators=(',', ': '), ensure_ascii=False)
+        data_file.write(to_unicode(str_))
+    
+    
+    # for kanji in kanji_list:
+    #     print(kanji["kanji"]["character"])
+    
+def read_news():
+    with io.open("news_data.json") as data_file:
+        data_loaded = json.load(data_file)
+
+   # print(data_loaded)
+    
+
+write_news()
+read_news()
